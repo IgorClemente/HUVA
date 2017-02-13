2d65ee8c2f036d61d59218aa7da5d74d85998f4e
Igor Clemente <igorelectrofest2011@hotmail.com>
2017-02-13 16:59:10 -0200

    Adicionando Arquivos Site HUVA

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HUVA</title>
+
+  <!-- LINKS CSS -->
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+  <!-- MENU -->
+  <nav class="navbar navbar-toggleable-sm navbar-light bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand text-white" href="#">HUVA</a>
+
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="index.html">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="soon.html">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="soon.html">Encontro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="soon.html">Sobre Nós</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="soon.html">Contato</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <!-- FIM MENU -->
+  
+  <!-- CONTENT -->
+  <div class="video-home">
+    <div class="home-content hidden-xs-down  text-center" >
+      <h1 class="text-white text-title" >Grupo H.U.V.A.</h1>
+      <hr  class="text-white" id="short">
+      <p class="text-white text-subtitle">20 anos de História, 20 anos de Luz</p>  
+      <div class="button-inscricao button-desk">
+        <a href="contact.php">Fazer inscrição</a>
+      </div>
+      
+    </div>
+
+    <div class="home-content-mobile hidden-sm-up text-center">
+      <h1 class="text-white text-title" style="font-size: 20px; color: #fff;">Grupo H.U.V.A.</h1>
+      <hr  class="text-white" id="short">
+      <p class="text-white text-subtitle" style="font-size: 20px; color: #fff;">20 anos de História, 20 anos de Luz</p>  
+      <div class="button-inscricao hidden-sm-up">
+        <a href="contact.php" style="font-size: 20px; color: #fff;">Fazer inscrição</a>
+      </div>
+    </div>
+    
+    <video id="video-bg" preload="auto" autoplay="true" loop="loop" muted="muted" class="embed-responsive-item" >
+      <source src="videos/54.mp4" type="video/mp4">
+    </video>
+
+
+  </div>
+  <!-- FIM CONTENT -->
+
+  <!-- CONTAINER -->
+    <div class="container-fluid">
+      
+      <div class="row">
+        <div class="col-md-6 col-sm-12 div-img">
+          <img class="img-fluid" alt="Blog" src="images/fundo arte.png"></img>
+        </div>
+        <div class="col-md-6 col-sm-12 div-txt text-center">
+          
+          <div class="font-weight-bold">
+            Data: 17,18 e 19/03/2017
+          </div> 
+          <div>
+            Local: <a class="font-weight-bold">Cidade dos Meninos (Casa do Cursilho) - Esquina da Rua Cáucaso com a Rua Batávia</a> - Santo André - SP
+          </div>    
+          <div>
+            <a class="font-weight-bold">Horário:</a> 20h00 do dia 17/03 (Sexta-Feira) - Entrada pela Rua Batávia, S/N.
+          </div> 
+           <div class="font-weight-bold">
+            Leve: roupa de cama, cobertor e obejtos de higiene pessoal.
+          </div>
+          <div>
+            <a class="font-weight-bold">Observação:</a> TODOS OS PARTICIPANTES DORMIRÃO NO LOCAL DO ENCONTRO (SEM EXCEÇÃO).
+          </div>  
+           <div>
+            <a class="font-weight-bold">Atenção:</a> SOMENTE AS PRIMEIRAS <a class="font-weight-bold">90 FICHAS </a>ENTREGUES, PARTICIPARÃO DO ENCONTRO.
+          </div> 
+          <div class="text-uppercase">
+            <a class="font-weight-bold">atenção:</a>   Teremos cantina no local</br>
+          </div>   
+          
+
+        </div> 
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 col-sm-12 div-txt text-center">
+        <div class="font-weight-bold text-danger">
+            IDADE MÍNIMA DE 13 ANOS
+          </div>    
+           <div class="font-weight-bold text-danger">
+            PREÇO: R$ 40,00 - As fichas e os cadastros só serão aceitos mediante pagamento</br>
+          </div> 
+            <div class="text-uppercase font-weight-bold text-danger">
+            é proibido o uso de celular e rádio. qualquer contato externo será feito pela organização do evento.
+          </div> 
+         <!-- <div class="font-weight-bold text-center">
+            INFORMAÇÕES: 
+            <div>9 8232 - 3267 | LEONARDO</div>
+            <div>9 7246 - 0033 | DANIELA</div>
+            <div>9 8574 - 5227 | JAIRO</div>
+            <div> ou acesse: <a href="https://www.facebook.com/ejchuva/" style="text-decoration: none;"> nossa página do Facebook</a></div>
+          </div> 
+          -->
+        </div> 
+        <div class="col-md-6 col-sm-12  div-img-right">
+          <img class="img-fluid" alt="Blog" src="images/poster.jpg"></img>
+        </div>
+      </div>
+
+<!-- OCULTO
+      <div class="row">
+        <div class="col-md-8 col-sm-12 div-img">
+          <img class="img-fluid" alt="Blog" src="images/1.jpg"></img>
+        </div>
+        <div class="col-md-4 col-sm-12 div-txt">
+          Leia nosso Blog
+        </div> 
+      </div>
+ 
+
+      <div class="row">
+        <div class="col-md-4 col-sm-12 div-txt">
+          Veja nossos encontros
+        </div> 
+        <div class="col-md-8 div-img-right">
+          <img class="img-fluid" alt="Blog" src="images/2.jpg"></img>
+        </div>
+      </div>
+-->      
+
+    </div>
+  <!-- FIM CONTAINER -->
+
+<!-- FOOTER -->
+  
+    
+      
+      <div class="col-md-12 footer-txt-title text-center space-top wid">
+        Quer saber mais sobre a gente e os nossos encontros?
+        <hr class="hr-color">
+      </div>
+      
+
+      <div class="row wid text-center" style="font-size: 25px;">
+             <div class="col-6">
+                <a class=" font-weight-bold">Contato:</a></br> 
+                Tel: 4472-8444</br>                       
+                Cel: 9 8232 - 3267</br>
+              </div>
+              <div class="col-6">
+                Nos siga nas redes sociais ;)
+                </br>
+                <a href="https://www.facebook.com/ejchuva/"><img src="images/icons/face1.png" class="logo"></a>
+               <a href="https://www.instagram.com/grupohuva/"><img src="images/icons/insta1.png" class="logo"></a>
+               <!-- <img src="images/icons/500.png" alt="" class="logo"> -->
+              </div> 
+
+           <!--    <div class="col-md-12">
+                Rua Oratório, 1458 
+              </div> 
+
+               <div class="col-md-12">
+                Parque das Nações, Santo André - SP
+              </div> 
+     -->          
+        </div>
+
+        
+
+        <div class="col-md-12 footer-txt-small text-center wid">
+        <hr class="hr-color">
+          Site desenvolvido por <a class="font-weight-bold">  Eduardo Marques</a>
+        </div>
+
+   
+  
+
+<!-- FOOTER -->
+
+
+
+
+  <script src="node_modules/jquery/dist/jquery.min.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
